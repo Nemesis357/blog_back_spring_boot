280cd272517e7e9046f5089340f146e6626a5cd2
Nemesis357 <nenadn89@gmail.com>
2018-04-10 09:26:06 +0200

    Service fix

--- src/main/java/com/nenadniko/service/MessageRepo.java
+++ src/main/java/com/nenadniko/service/MessageRepo.java
@@ -9,9 +9,4 @@ import com.nenadniko.entity.Message;
 @Repository
 public interface MessageRepo extends JpaRepository<Message, Long> {
 	Message findById(Integer id);
-	//@Query("UPDATE spring_db.message SET date = :date, email = :email, message = :message, name = :name WHERE id = :id")
-	//Message updateMessage(Message msg);
-	
-	 //@Query("SELECT a FROM Article a WHERE a.title=:title and a.category=:category")
-	 //PostDB post findPostById(@Param("uniqueKey") String title);
 }
